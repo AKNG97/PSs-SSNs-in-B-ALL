804dabed2543e4a74c502c6aadc1c60148463683
Arturo Kenzuke <72327195+AKNG97@users.noreply.github.com>
2025-03-03 18:07:01 -0600

    Update ClusteringAnalysis.R

--- ClusteringAnalysis.R
+++ ClusteringAnalysis.R
@@ -15,170 +15,75 @@ Lioness_BALL_TARGET = read.csv("/storage/kuijjerarea/akng/PseudogenesAnalysis/Ha
 
 Cancer_GCN <- readRDS("/storage/kuijjerarea/akng/PseudogenesAnalysis/HandleReps/ExtCI/Bootsrtrapping/BALL_GCN_Sp_sequential.RDS")
 
-annotPS <- readRDS("/storage/kuijjerarea/akng/PseudogenesAnalysis/HandleReps/ExtCI/annot_PS.RDS")
-annot <- readRDS("/storage/kuijjerarea/akng/PseudogenesAnalysis/HandleReps/ExtCI/annot_TCGA.RDS")
+annot <- read.delim("gene_annotation_TCGA.tsv")
+
+annot_PS <- annot_TCGA[intersect(grep(".*pseudogene", annot_TCGA$gene_type), 
+                                 grep(".*pseudogene", annot_TCGA$Type)),]
+
 Cancer_GCN <- Cancer_GCN %>% filter(Source %in% annotPS$HGNC_symbol &
                                       Target %in% annotPS$HGNC_symbol)
 
+#Get network without Ps-Ps edges
 Lioness_BALL_TARGET1 = Lioness_BALL_TARGET[!(rownames(Lioness_BALL_TARGET) %in%
                                               Cancer_GCN$ID),]
 
+#Get network with Ps-Ps edges
 Lioness_BALL_TARGET2 = Lioness_BALL_TARGET[rownames(Lioness_BALL_TARGET) %in%
                                                Cancer_GCN$ID,]
 
-filtered_results_TARGET <- featurefilter(Lioness_BALL_TARGET1, percentile=1508*100/39082,
+#Filter network1
+filtered_results_TARGET1 <- featurefilter(Lioness_BALL_TARGET1, percentile=1508*100/39082,
+                                         method='var', topN=5)
+#Filter network2
+filtered_results_TARGET2 <- featurefilter(Lioness_BALL_TARGET2, percentile=25,
                                          method='var', topN=5)
 
-filtered_results_TARGET <- featurefilter(Lioness_BALL_TARGET2, percentile=25,
-                                         method='var', topN=5)
+Lioness_BALL_TARGET_scaled1 <- as.data.frame(t(scale(t(filtered_results_TARGET1$filtered_data))))
+Lioness_BALL_TARGET_scaled2 <- as.data.frame(t(scale(t(filtered_results_TARGET1$filtered_data))))
 
-Lioness_BALL_TARGET_scaled <- as.data.frame(t(scale(t(filtered_results_TARGET$filtered_data))))
+r.hc_TARGET <- M3C(Lioness_BALL_TARGET_scaled2, method=1, clusteralg = "hc", cores = 30, maxK = 10, seed = 123)
 
-r.hc_TARGET <- M3C(Lioness_BALL_TARGET_scaled, method=1, clusteralg = "hc", cores = 30, maxK = 10, seed = 123)
+ggsave("pval_M3C_BALL_Target_PS.png", plot = r.hc_TARGET$plots[[3]], dpi = 300)
+ggsave("RCSI_M3C_BALL_Target_PS.png", plot = r.hc_TARGET$plots[[4]], dpi = 300)
 
-ggsave("pval_M3C_BALL_Target_PS_paper.png", plot = r.hc_TARGET$plots[[3]], dpi = 300)
-ggsave("RCSI_M3C_BALL_Target_PS_paper_2.png", plot = r.hc_TARGET$plots[[4]], dpi = 300)
-
-CI.BALL_TARGET <- readRDS("/storage/kuijjerarea/akng/PseudogenesAnalysis/HandleReps/ExtCI/CI_primaryBM_BALL.RDS")
 #TARGET
 assigments.TARGET <- r.hc_TARGET$realdataresults[[2]]$ordered_annotation
 assigments.TARGET <- assigments.TARGET %>% mutate(Sample = rownames(assigments.TARGET))
-#saveRDS(assigments.TARGET, "Final/assigmentsTARGET_noPS_1508.RDS")
+saveRDS(assigments.TARGET, "assigmentsTARGET_PS_25pMostVariable.RDS")
 
-# 
-CI.BALL_TARGET$patient <- gsub("-", ".", CI.BALL_TARGET$patient)
-# #ci.all$patient <- gsub("-", ".", ci.all$patient)
-CI.BALL_TARGET <- CI.BALL_TARGET %>% inner_join(assigments.TARGET, by = c("patient" = "Sample"))
+Lioness_BALL_TARGET_scaled <- as.data.frame(t(scale(t(Lioness_BALL_TARGET))))
+assigments.TARGET <- readRDS("assigmentsTARGET_PS_25pMostVariable.RDS")
+colnames(Lioness_BALL_TARGET_scaled) <- gsub("-", ".", colnames(Lioness_BALL_TARGET_scaled))
+Lioness_BALL_TARGET_pivot <- Lioness_BALL_TARGET_scaled %>% mutate(Edge = rownames(Lioness_BALL_TARGET_scaled)) %>% 
+  pivot_longer(!Edge, names_to = "Sample", values_to = "Z_score_Lioness") %>% 
+  inner_join(assigments.TARGET, by = c("Sample" = "Sample"))
 
-SurvCI <- data.frame(Sample = CI.BALL_TARGET$patient,
-                     Vital_status = CI.BALL_TARGET$VS_update,
-                     DLF = CI.BALL_TARGET$OS_time_update,
-                     consensuscluster.Full = CI.BALL_TARGET$consensuscluster)
+diff_TARGET <- Lioness_BALL_TARGET_pivot %>% 
+  group_by(Edge) %>% 
+  dplyr::summarize(Median1 = median(Z_score_Lioness[consensuscluster == 1]),
+                   Median2 = median(Z_score_Lioness[consensuscluster == 2]),
+                   Median3 = median(Z_score_Lioness[consensuscluster == 3]),
+                   Diff_1vs2 = median(Z_score_Lioness[consensuscluster == 1]) - median(Z_score_Lioness[consensuscluster == 2]),
+                   Diff_1vs3 = median(Z_score_Lioness[consensuscluster == 1]) - median(Z_score_Lioness[consensuscluster == 3]),
+                   Diff_2vs3 = median(Z_score_Lioness[consensuscluster == 2]) - median(Z_score_Lioness[consensuscluster == 3]),
+                  pval.Cls1_vs2 = wilcox.exact(Z_score_Lioness[consensuscluster == 1],
+                                                Z_score_Lioness[consensuscluster == 2])$p.value,
+                   pval.Cls1_vs3 = wilcox.exact(Z_score_Lioness[consensuscluster == 1],
+                                                Z_score_Lioness[consensuscluster == 3])$p.value,
+                   pval.Cls2_vs3 = wilcox.exact(Z_score_Lioness[consensuscluster == 2],
+                                                Z_score_Lioness[consensuscluster == 3])$p.value
+  ) %>%
+ mutate(adj.pval_1vs2 =  p.adjust(pval.Cls1_vs2, method = "fdr"),
+         adj.pval_1vs3 =  p.adjust(pval.Cls1_vs3, method = "fdr"),
+         adj.pval_2vs3 =  p.adjust(pval.Cls2_vs3, method = "fdr"))
 
-SurvCI <- data.frame(Sample = CI.BALL_TARGET$patient,
-                     Vital_status = CI.BALL_TARGET$VS_update,
-                     DLF = CI.BALL_TARGET$OS_time_update,
-                     consensuscluster = CI.BALL_TARGET$consensuscluster)
+diff_TARGET %>% filter(adj.pval_2vs3 < 0.05) %>% group_by(Edge) %>% 
+  summarise(diff = Median2 - Median3) %>%
+  dplyr::count(pos = diff > 0.5,
+               neg = diff < -0.5)
 
-SurvCI$Vital_status[SurvCI$Vital_status == "Alive"] <- 0
-SurvCI$Vital_status[SurvCI$Vital_status == "Dead"] <- 1
-SurvCI$Vital_status <- as.numeric(SurvCI$Vital_status)
+diff_TARGET <- diff_TARGET %>% inner_join(Cancer_GCN[,c(1,2,4)], by = c("Edge" = "ID"))
 
-SurvCI$Vital_status[SurvCI$DLF > 1825] <- 0
-SurvCI$DLF[SurvCI$DLF > 1825] <- 1825
+write.csv(diff_TARGET, "DiffCoExpr_TARGET_25p.csv", quote = F, row.names = F)
 
-survfit_object <- survdiff(Surv(DLF, Vital_status) ~ consensuscluster, data = SurvCI)
-# 
-# survminer::pairwise_survdiff(Surv(DLF, Vital_status) ~ consensuscluster,
-#                             data = SurvCI)
-
-print(survfit_object$strata)
-
-colores <- c("consensuscluster=1" = "#619CFF", 
-             "consensuscluster=2" = "#F8766D", 
-             "consensuscluster=3" = "#00BA38")
-surv.plot.BALL.lioness <- survfit2(Surv(DLF, Vital_status) ~ consensuscluster, data = SurvCI) %>%
-  ggsurvfit(size = 1.5) +
-  labs(
-    x = "Days to last follow up",
-    y = "Overall survival probability"
-  ) + add_pvalue() + theme(axis.text=element_text(size=12),
-                                         axis.title=element_text(size=14)) +
-  scale_color_manual(values = c("#F8766D", "#619CFF", "#00BA38")) +
-  add_risktable(
-    risktable_height = 0.2,
-    size = 4, # increase font size of risk table statistics
-    theme =   # increase font size of risk table title and y-axis label
-      list(
-        theme_risktable_default(axis.text.y.size = 12, 
-                                plot.title.size = 12),
-        theme(plot.title = element_text(face = "bold"))
-      )
-  )
-
-ggsave("KM_TARGET_Full_paper_2.png", surv.plot.BALL.lioness, height = 10, width = 7, dpi = 300)
-
-
-default_colors <- ggplot_build(surv.plot.BALL.lioness)$data[[1]]$colour
-print(default_colors)
-
-# 
-
-##### MP2 ##### 
-#/storage/kuijjerarea/akng/PseudogenesAnalysis/HandleReps/ExtCI/new_analysis/analyze2
-#module load R/4.2.1-foss-2022a
-
-Lioness_BALL_MP2 = data.table::fread("/storage/kuijjerarea/akng/PseudogenesAnalysis/HandleReps/ExtCI/MP2PRT/CorrectCI/Full_Correct/LionessPS_BALL_MP2_Full_CheckError.csv")
-Lioness_BALL_MP2 <- Lioness_BALL_MP2 %>% as.data.frame
-rownames(Lioness_BALL_MP2) <- Lioness_BALL_MP2$Edge_ID
-Lioness_BALL_MP2 <- Lioness_BALL_MP2[,-1]
-
-Cancer_GCN <- readRDS("/storage/kuijjerarea/akng/PseudogenesAnalysis/HandleReps/ExtCI/MP2PRT/CorrectCI/Full_Correct/BALL_MP2_GCN_SignifEdges_2ndRun.RDS")
-
-annotPS <- readRDS("/storage/kuijjerarea/akng/PseudogenesAnalysis/HandleReps/ExtCI/annot_PS.RDS")
-annot <- readRDS("/storage/kuijjerarea/akng/PseudogenesAnalysis/HandleReps/ExtCI/annot_TCGA.RDS")
-Cancer_GCN1 <- Cancer_GCN %>% filter(Source %in% annotPS$HGNC_symbol &
-                                       Target %in% annotPS$HGNC_symbol)
-
-Lioness_BALL_MP21 = Lioness_BALL_MP2[!(rownames(Lioness_BALL_MP2) %in%
-                                         Cancer_GCN1$ID),]
-
-Lioness_BALL_MP23 = Lioness_BALL_MP2[rownames(Lioness_BALL_MP2) %in%
-                                       Cancer_GCN1$ID,]
-
-filtered_results_MP2 <- featurefilter(Lioness_BALL_MP23, percentile=4811*100/19244, method='var', topN=5)
-filtered_results_MP2 <- featurefilter(Lioness_BALL_MP21, percentile=4811*100/505650, method='var', topN=5)
-filtered_results_MP2 <- featurefilter(Lioness_BALL_MP2, percentile=4811*100/524894, method='var', topN=5)
-
-Lioness_BALL_MP23_scaled <- as.data.frame(t(scale(t(filtered_results_MP2$filtered_data))))
-#Lioness_BALL_MP2_scaled <- as.data.frame(t(scale(t(Lioness_BALL_MP21))))
-# 
-# Lioness_BALL_MP23_scaled <- as.data.frame(t(scale(t(Lioness_BALL_MP23))))
-# 
-# filtered_results_MP21 <- featurefilter(Lioness_BALL_MP21, percentile=2000*100/505650, 
-#                                        method='var', topN=5)
-#Lioness_BALL_MP21_scaled <- as.data.frame(t(scale(t(filtered_results_MP21$filtered_data))))
-
-r.hc_MP2 <- M3C(Lioness_BALL_MP23_scaled, method=1, clusteralg = "hc", cores = 70, maxK = 10, seed = 123)
-#r.hc_MP2 <- M3C(filtered_results_MP2$filtered_data, method=1, clusteralg = "spectral", cores = 80, maxK = 5, seed = 123)
-
-CI.BALL_MP2 <- readRDS("/storage/kuijjerarea/akng/PseudogenesAnalysis/HandleReps/ExtCI/MP2PRT/CorrectCI/Full_Correct/CI_MP2PRT_FULL.RDS")
-assigments.MP2 <- r.hc_MP2$realdataresults[[2]]$ordered_annotation
-assigments.MP2 <- assigments.MP2 %>% mutate(Sample = rownames(assigments.MP2))
-#saveRDS(assigments.MP2, "Final/assigmentsMP2_Full_4811.RDS")
-saveRDS(SurvCI, "assigmentsMP2_PS_25p.RDS")
-
-#KM
-CI.BALL_MP2$sample_submitter_id <- gsub("-", ".", CI.BALL_MP2$sample_submitter_id)
-CI.BALL_MP2 <- CI.BALL_MP2 %>% inner_join(assigments.MP2, by = c("sample_submitter_id" = "Sample"))
-
-SurvCI <- data.frame(Sample = CI.BALL_MP2$sample_submitter_id,
-                     Vital_status = CI.BALL_MP2$OS_Status,
-                     DLF = CI.BALL_MP2$OS_Time,
-                     consensuscluster = CI.BALL_MP2$consensuscluster)
-
-SurvCI$Vital_status[SurvCI$DLF > 1825] <- 0
-SurvCI$DLF[SurvCI$DLF > 1825] <- 1825
-
-survdiff(Surv(DLF, Vital_status) ~ consensuscluster, data = SurvCI)
-
-surv.plot.BALL.lioness <- survfit2(Surv(DLF, Vital_status) ~ consensuscluster, data = SurvCI) %>% 
-  ggsurvfit(size = 1.5) +
-  labs(
-    x = "Days to last follow up",
-    y = "Overall survival probability"
-  ) + add_pvalue() + theme(axis.text=element_text(size=12),
-                                         axis.title=element_text(size=14)) +
-  add_risktable(
-    risktable_height = 0.2,
-    size = 4, # increase font size of risk table statistics
-    theme =   # increase font size of risk table title and y-axis label
-      list(
-        theme_risktable_default(axis.text.y.size = 12, 
-                                plot.title.size = 12),
-        theme(plot.title = element_text(face = "bold"))
-      )
-  )
-
-ggsave("KM_MP2PRT_Full_4811.png", surv.plot.BALL.lioness, height = 10, width = 7, dpi = 300)
 
